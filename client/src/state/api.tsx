60987753feea989dd6dcfabd364943fec33742b0
Body Moud <AbdallaEl7dad1@Abdallas-MacBook-Pro.local>
2023-08-18 18:33:44 +0200

    ll

--- client/src/state/api.tsx
+++ client/src/state/api.tsx
@@ -2,7 +2,7 @@ import {createApi , fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import { GetKpisResponse, GetProductsResponse, GetTransactionsResponse } from './types'
 
 export const api = createApi({
-    baseQuery :fetchBaseQuery({baseUrl :import.meta.env.VITE_BASE_URL}) , 
+    baseQuery :fetchBaseQuery({baseUrl :import.meta.env.VITE_BASE_UR ||"https://finance-q08o.onrender.com/"}) , 
     reducerPath : "main" , 
     tagTypes : ["Kpis" , "Products" , "Transactions"] ,
     endpoints: (build) => ({
